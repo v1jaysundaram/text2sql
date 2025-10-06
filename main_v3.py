5ffe5cd8de3b4afe331d73d630e8cf4c91efa897
Vijay Sundaram Mohana <vijaysundaram0206@gmail.com>
2025-10-06 18:41:36 +0100

    used json kb instead of further conversion to str

---
+++ main_v3.py
@@ -0,0 +1,40 @@
+# -------------------------------
+# Text-to-SQL (v3)
+# -------------------------------
+
+# Import Libraries
+from dotenv import load_dotenv
+from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
+import json
+
+# Load environment variables
+load_dotenv()  
+
+# Initialize the LLM
+#llm = ChatOllama(model="sqlcoder")
+llm = ChatOpenAI()
+
+# Load the knowledge base
+with open("kb_final.json", "r", encoding="utf-8") as f:
+    KB = json.load(f)
+
+# Prompt Template
+prompt = f"""
+You are a SQL assistant. Given the schema below, generate a valid SQL query for the question in the given dialect. 
+Output only the SQL query, nothing else.
+
+Dialect: MySQL 
+
+Schema:
+{KB}
+
+Question:
+How many customers are there in the state of 'Minas Gerais'?
+"""
+
+# Invoke the LLM
+response = llm.invoke(prompt)
+
+print(response.content)
+
